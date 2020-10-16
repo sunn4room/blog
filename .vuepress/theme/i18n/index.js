1b5f30bd0ed3eec0ba07cb07f2516e67121086a4
sunn4room <1795999147@qq.com>
2020-10-16 11:10:19 +0800

    2.0

--- .vuepress/theme/i18n/index.js
+++
@@ -1,9 +0,0 @@
-let ctx = require.context('.', false, /.js$/)
-let ept = {}
-
-ctx.keys().forEach(k => {
-  if (k === './index.js') return
-  ept[k.substring(2, k.length - 3)] = ctx(k).default
-})
-
-export default ept
